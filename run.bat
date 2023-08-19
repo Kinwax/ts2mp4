a90222765d0392d3b57e7ce20eec4d8e47268d32
Kenneth Wong <54704803+Kinwax@users.noreply.github.com>
2023-08-20 00:23:46 +0800

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,4 +1,25 @@
-echo off
+@echo off
 cls
-for %%a in ("*.ts") do .\ffmpeg -i "%%a" -f mp4 -codec copy "%%~na.mp4
+
+setlocal enabledelayedexpansion
+
+REM 逐个处理传递给脚本的文件参数
+:process_files
+if "%~1" == "" goto end_process
+
+REM 提取文件名和扩展名
+set "fullpath=%~1"
+for %%a in ("!fullpath!") do (
+    set "filename=%%~na"
+    set "extension=%%~xa"
+    set "filedir=%%~dpa"
+)
+
+REM 使用 ffmpeg 处理文件
+.\ffmpeg -i "!fullpath!" -f mp4 -codec copy "!filedir!!filename!.mp4"
+
+shift
+goto process_files
+
+:end_process
 pause
